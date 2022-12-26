a76451592a0ed9c12fb2782d6bffcb01a70493e9
Gaston Avogadro <gaston.avogadro@gmail.com>
2022-12-26 19:33:37 -0300

    testimonials, contact and footer created

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// BURGER MENU
+
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 
@@ -12,3 +14,26 @@ document.querySelectorAll('.nav-link').forEach((n) =>
         navMenu.classList.remove('active');
     })
 );
+
+// TESTIMONIALS SLIDERS
+
+const swiper = new Swiper('.slide-content', {
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        
+        850: {
+            slidesPerView: 3,
+        },
+    },
+});
